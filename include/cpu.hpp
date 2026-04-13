97cabe119a42a4dbad62924eed615f8d8e87c77d
Sam Coburn <samcoburn@Mac.lan>
2026-04-13 15:08:58 -0400

    Implementing ADC 8-bit Arithmetic Instruction

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -7,7 +7,7 @@ enum class ArithmeticTarget {
 };
 
 enum class InstructionType {
-    ADD
+    ADD, ADC
 };
 
 struct Instruction {
@@ -18,9 +18,10 @@ struct Instruction {
 class CPU {
     public:
         Registers registers;
-
         void execute(const Instruction& instruction) {
             switch (instruction.type) {
+
+                /* 8-bit Arithmetic Operations */
 
                 // ADD Instruction adds specified register contents to register A
                 case InstructionType::ADD:
@@ -56,7 +57,40 @@ class CPU {
 
                             registers.a = new_value;
                     }
-                    break;
+
+                case InstructionType::ADC:
+                    switch (instruction.target) {
+                        case ArithmeticTarget::B:
+                            uint8_t value = registers.b;
+                            uint8_t new_value = adc(value);
+
+                            registers.a = new_value;
+                        case ArithmeticTarget::C:
+                            uint8_t value = registers.c;
+                            uint8_t new_value = adc(value);
+
+                            registers.a = new_value;
+                        case ArithmeticTarget::D:
+                            uint8_t value = registers.d;
+                            uint8_t new_value = adc(value);
+
+                            registers.a = new_value;
+                        case ArithmeticTarget::E:
+                            uint8_t value = registers.e;
+                            uint8_t new_value = adc(value);
+
+                            registers.a = new_value;
+                        case ArithmeticTarget::H:
+                            uint8_t value = registers.h;
+                            uint8_t new_value = adc(value);
+
+                            registers.a = new_value;
+                        case ArithmeticTarget::L:
+                            uint8_t value = registers.l;
+                            uint8_t new_value = adc(value);
+
+                            registers.a = new_value;
+                    }
 
                 default:
                     // TODO: support more instructions
@@ -76,6 +110,18 @@ class CPU {
             // together result in a value bigger than 0xF. If the result is larger than 0xF
             // than the addition caused a carry from the lower nibble to the upper nibble.
             registers.f.half_carry = (registers.a & 0xF) + (value & 0xF) > 0xF;
+
+            return result;
+        }
+
+        uint8_t adc(uint8_t value) {
+            uint8_t result = registers.a + value + registers.f.carry;
+            bool overflowed = result < registers.a;
+
+            registers.f.zero = result == 0;
+            registers.f.subtract = false;
+            registers.f.carry = overflowed;
+            registers.f.half_carry = (registers.a & 0xF) + (value & 0xF) > 0xF; // Same logic as for add()
 
             return result;
         }
